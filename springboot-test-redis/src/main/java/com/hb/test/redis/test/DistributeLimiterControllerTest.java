326ac6900797739ed58e40289767cd0cd4259cc7
黄彪 <191059014@qq.com>
2020-05-04 09:47:00 +0800

    利用redis模块测试分布式限流

--- springboot-test-redis/src/main/java/com/hb/test/redis/test/DistributeLimiterControllerTest.java
+++ springboot-test-redis/src/main/java/com/hb/test/redis/test/DistributeLimiterControllerTest.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * ========== Description ==========
+ * ========== 分布式锁测试 ==========
  *
  * @author Mr.huang
  * @version com.hb.test.redis.test.Test.java, v1.0
@@ -22,15 +22,16 @@ public class DistributeLimiterControllerTest {
     @GetMapping("/test")
     @RedisEasyLimiter(key = "test", period = 10, maxTimes = 3)
     public void test() {
-        System.out.println(111);
+        System.out.println("pass");
     }
 
     @GetMapping("/test1")
     public void test1() {
-        if (redisLimiter.pass("",10,3)) {
-
+        if (!redisLimiter.pass("test", 10, 3)) {
+            System.out.println("not pass");
+            return;
         }
-        System.out.println(111);
+        System.out.println("pass");
     }
 
 }
