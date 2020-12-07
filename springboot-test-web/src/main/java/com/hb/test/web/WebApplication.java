50aa2525b009ce1d652a7f651e7756f0e98f4215
黄彪 <191059014@qq.com>
2020-12-07 09:22:19 +0800

    rocketmq

---
+++ springboot-test-web/src/main/java/com/hb/test/web/WebApplication.java
@@ -0,0 +1,18 @@
+package com.hb.test.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @version v0.1, 2020/8/4 19:51, create by huangbiao.
+ */
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+
+}
