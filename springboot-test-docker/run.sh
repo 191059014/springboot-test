cc1bd2d7d2eb58b93946780312ff4082c835cc29
黄彪 <191059014@qq.com>
2021-01-18 17:04:53 +0800

    docker

--- springboot-test-docker/run.sh
+++ springboot-test-docker/run.sh
@@ -4,5 +4,5 @@ java -server -Xmx3000m -Xms3000m -Xmn2000m -Xss512k -XX:+DisableExplicitGC \
      -XX:LargePageSizeInBytes=128m -XX:+UseFastAccessorMethods \
      -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 \
      -Dspring.profiles.active=$profiles_active \
-     -jar /app/springbootTestDocker.jar
+     -jar /app/springboottestdocker.jar
 
