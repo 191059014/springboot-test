b297d82e6a3621dae020c70c8e0f2e0bc18b46cc
黄彪 <191059014@qq.com>
2021-01-27 09:44:32 +0800

    docker

--- springboot-test-docker/src/main/java/com/hb/test/docker/controller/TestController.java
+++ springboot-test-docker/src/main/java/com/hb/test/docker/controller/TestController.java
@@ -1,5 +1,9 @@
 package com.hb.test.docker.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +17,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/controller/test")
 public class TestController {
 
+    /**
+     * 日志
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
+
+    @Autowired
+    private Environment env;
+
     @GetMapping("/testDocker")
     public Object testDocker() {
         return "this is docker server";
+    }
+
+    @GetMapping("/print/environment")
+    public void printEnvironment() {
+        String profilesActive = env.getProperty("spring.profiles.active");
+        LOGGER.info("profilesActive={}", profilesActive);
     }
 
 }
